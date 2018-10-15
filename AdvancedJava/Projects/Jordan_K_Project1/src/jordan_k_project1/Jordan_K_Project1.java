e7eacebd0c0ee8b939db831c6c665a59b75212f5
thewhitefu <jordanadamc@gmail.com>
2018-10-14 21:06:45 -0500

    fixed project 1

--- AdvancedJava/Projects/Jordan_K_Project1/src/jordan_k_project1/Jordan_K_Project1.java
+++ AdvancedJava/Projects/Jordan_K_Project1/src/jordan_k_project1/Jordan_K_Project1.java
@@ -10,9 +10,19 @@ public class Jordan_K_Project1 {
     //In your main program, search the array and display all of the elements 
     //that begin with 'M'.
     public static void main(String[] args) {
+        
+        String userInput = "s";
         String[] names = {"Bill", "Mike", "Joe", "Dave", "Sam", "Mary", 
             "George"};
         
-    }
-    
+        
+            for (int i = 0; i < names.length; i++) 
+            if (names[i].trim().toLowerCase().startsWith(userInput.toLowerCase())==true)
+            {                
+                System.out.println(names[i].toString());
+            }
+       
+    }  
 }
+    
+
